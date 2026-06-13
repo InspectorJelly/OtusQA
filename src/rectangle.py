bf1318266a6d64ab729ed8127ec962ad52abba31
InspectorJelly <105738258+InspectorJelly@users.noreply.github.com>
2026-06-13 19:06:34 +0300

    fix message

--- src/rectangle.py
+++ src/rectangle.py
@@ -3,6 +3,8 @@ from src.figure import Figure
 
 class Rectangle(Figure):
     def __init__(self, side_a, side_b):
+        if side_a <= 0 or side_b <= 0:
+            raise ValueError("Стороны фигуры должны быть положительными числами")
         self.side_a = side_a
         self.side_b = side_b
 
